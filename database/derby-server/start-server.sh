e79c145169d8c219bea9be1250e8be72aa030f6b
Harsimran <kaur.harsimran301@gmail.com>
2022-05-19 19:44:45 -0600

    [feat]: local changes

--- database/derby-server/start-server.sh
+++ database/derby-server/start-server.sh
@@ -1,3 +1,4 @@
-java -cp ./lib/derbynet.jar org.apache.derby.drda.NetworkServerControl start -h 0.0.0.0 -p 51527
+java -cp ./lib/derbynet.jar org.apache.derby.drda.Netwoorg.apache.derby.drda.NetworkServerControlrkServerControl start -h 0.0.0.0 -p 51527
+#java -cp ./lib/derbynet.jar org.apache.derby.drda.Netwoorg.apache.derby.drda.NetworkServerControlrkServerControl start -h 0.0.0.0 -p 51527
 
 
